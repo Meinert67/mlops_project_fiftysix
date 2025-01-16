53e78ae00e6ff33fd00e078049ad59e1ec320e00
reehdemption <reehjakob@gmail.com>
2025-01-16 16:04:54 +0100

    Added Unit Testing

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,34 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import torch
 from torch.utils.data import Dataset
+from src.project.data import preprocess
 
-from project.data import MyDataset
+
+
 
 
 def test_my_dataset():
     """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
+    dataset = preprocess()
     assert isinstance(dataset, Dataset)
+
+    train = dataset.train_set
+    test = dataset.test_set
+
+    assert len(train) == 50000, "Train Dataset did not have the correct number of samples (50.000)"
+    assert len(test) == 10000, "Test Dataset did not have the correct number of samples (10.000)"
+    for dataset in [train, test]:
+        for x, y in dataset:
+            assert x.shape == (3, 32, 32), "Dataset shape not correct size"
+            assert y in range(10)
+    train_targets = set([y for _, y in train])
+    assert train_targets == set(range(10))
+    
+    test_targets = set([y for _, y in test])
+    assert test_targets == set(range(10))
+
+
+
+if __name__ == "__main__":
+    test_my_dataset()
---
+++ tests/test_model.py
@@ -0,0 +1,15 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import torch
+from src.project.model import MyAwesomeModel
+
+def test_model():
+    model = MyAwesomeModel()
+    x = torch.randn(1, 3, 32, 32)
+    y = model(x)
+    assert y.shape == (1, 10), "Wrong output shape, should be (1, 10)"
+
+
+if __name__ == "__main__":
+    test_model()
