a87aadba0c2677a8fb1f1181363193e54209fd40
Meinert67 <39194488+Meinert67@users.noreply.github.com>
2025-01-23 14:50:17 +0000

    Pre-commit fixes

--- tests/integrationtests/test_apis.py
+++ tests/integrationtests/test_apis.py
@@ -3,11 +3,12 @@ import sys
 import os
 
 # Add the directory two levels up to the system path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 from src.project.api import app
 
 client = TestClient(app)
+
 
 def test_read_root():
     with TestClient(app) as client:
