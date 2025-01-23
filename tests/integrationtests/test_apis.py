aec3244bd61da91ef370c2cd426976a215c416ff
unknown <chrmeinert123@gmail.com>
2025-01-23 15:49:56 +0100

    Api testing

---
+++ tests/integrationtests/test_apis.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+import sys
+import os
+
+# Add the directory two levels up to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+from src.project.api import app
+
+client = TestClient(app)
+
+def test_read_root():
+    with TestClient(app) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"message": "Welcome to the model inference API!"}
